a6b8130475d0c0b2be73c933f9c8e132d7bdf771
alax <alax@localhost.localdomain>
2015-03-13 18:28:58 +0800

    compatible

---
+++ logic/src/define/ProtocolID.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   FrameworkProtocolID.h
+ * Author: alax
+ *
+ * Created on March 11, 2015, 10:38 AM
+ */
+
+#ifndef __ProtocolID_h__
+#define	__ProtocolID_h__
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    namespace Protocol {
+        enum {
+            
+        };
+    }
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* __ProtocolID_h__ */
+
